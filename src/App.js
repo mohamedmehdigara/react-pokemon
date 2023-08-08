f00e81b6abf6390008ec569a38af01990246c269
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-08 09:18:29 +0100

    edited App.js

--- src/App.js
+++ src/App.js
@@ -1,70 +1,67 @@
-
 import { useQuery } from '@apollo/react-hooks';
 import gql from "graphql-tag";
 import React from 'react';
-import App from "./App";
-import { render } from '@testing-library/react';
 
 const GET_POKEMON_INFO = gql`
 {
     pokemons(first: 150) {
       id
       number
-      name,
-      image,
+      name
+      image
       evolutions {
-        id,
-        number,
-        name,
+        id
+        number
+        name
         image
       }
     }
   }`
 
-  export default  App() ;{
-    const { data, loading, error } = useQuery(GET_POKEMON_INFO);
-  
-    if (loading) render( <p>Loading...</p>);
-    if (error)   render(<p>Error</p>);
-    render();{
-      
-    return <React.Fragment>
-        <h1>Pokémons</h1>
-    
-        <p>
-          <a href="https://en.wikipedia.org/wiki/List_of_Pok%C3%A9mon">
-            The Pokémon franchise
-          </a>{" "}
-          revolves around 832 fictional species of collectible monsters, each having
-          unique designs and skills. Conceived by Satoshi Tajiri in early 1989,
-          Pokémon are creatures that inhabit the fictional Pokémon World. This is
-          the list of the first 150 Pokémon as they appear in Pokémon Stadium,
-          starting with Bulbasaur in the top left corner and ending with Mewtwo in
-          the bottom right corner.
-        </p>
-        <div className="container">
-          {data &&
-            data.pokemons &&
-            data.pokemons.map((pokemon, index) => (
-              <div key={index} className="card">
-                <img src={pokemon.image}  alt={pokemon}/>
-                <div class="card-body">
-                  <h3>{pokemon.name}</h3>
+function App() {
+  const { data, loading, error } = useQuery(GET_POKEMON_INFO);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
+  return (
+    <React.Fragment>
+      <h1>Pokémons</h1>
+
+      <p>
+        <a href="https://en.wikipedia.org/wiki/List_of_Pok%C3%A9mon">
+          The Pokémon franchise
+        </a>{" "}
+        revolves around 832 fictional species of collectible monsters, each having
+        unique designs and skills. Conceived by Satoshi Tajiri in early 1989,
+        Pokémon are creatures that inhabit the fictional Pokémon World. This is
+        the list of the first 150 Pokémon as they appear in Pokémon Stadium,
+        starting with Bulbasaur in the top left corner and ending with Mewtwo in
+        the bottom right corner.
+      </p>
+      <div className="container">
+        {data &&
+          data.pokemons &&
+          data.pokemons.map((pokemon, index) => (
+            <div key={index} className="card">
+              <img src={pokemon.image} alt={pokemon.name} />
+              <div className="card-body">
+                <h3>{pokemon.name}</h3>
+                {pokemon.evolutions && pokemon.evolutions.length !== 0 && (
                   <p>
-                    {pokemon.evolutions && pokemon.evolutions.length !== 0 && (
-                      <p>
-                        {" "}
-                        Evolutions:
-                        {pokemon.evolutions.map((e, indx) => {
-                          return <p key={indx}> {e.name} </p>;
-                        })}
-                      </p>
-                    )}
+                    {" "}
+                    Evolutions:
+                    {pokemon.evolutions.map((e, indx) => {
+                      return <p key={indx}> {e.name} </p>;
+                    })}
                   </p>
-                </div>
+                )}
               </div>
-            ))}
-        </div>
-      </React.Fragment> 
-    
-                      }}
+            </div>
+          ))}
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
